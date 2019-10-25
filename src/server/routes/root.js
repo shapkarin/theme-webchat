eed152a90d96c4eeac0d47a2c0c1a55e23b6dc53
aburd <burdick.aaron@gmail.com>
2019-10-25 23:47:32 +0900

    add styles to markdown info page

--- src/server/routes/root.js
+++ src/server/routes/root.js
@@ -9,7 +9,12 @@ const showdown = require('showdown'),
 router.get('/', function (req, res) {
   fs.readFile(path.join(__dirname, '../intro.md'), 'utf8', (err, mdText) => {
     if (err) console.error(err)
-    res.send(converter.makeHtml(mdText))
+    res.send(`
+      <style>.container { width: 80%; max-width: 1200px; margin: auto }</style>
+      <div class="container">
+        ${converter.makeHtml(mdText)}
+      </div>
+    `)
   })
 })
 
