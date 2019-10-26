79eb2862af392df6ee1a92c3dc8681dd65fbb373
aburd <burdick.aaron@gmail.com>
2019-10-26 11:34:19 +0900

    move to customizable text message styles

--- src/client/components/TextMessage.tsx
+++ src/client/components/TextMessage.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Message } from '@src/types'
 
-interface Props extends Message {}
+interface Props extends Message {
+  user?: {
+    style: any
+  }
+}
+
+const defaultStyles = {
+  padding: "10px 20px",
+  border: "1px solid #666",
+  background: "#eee",
+  borderRadius: "20px"
+}
 
 const TextMessage: React.SFC<Props> = ({ text }) => {
   return (
     <div className="message text-message">
-      <span className="text-message-inner">
+      <span className="text-message-inner" style={defaultStyles}>
         {text}
       </span>
     </div>
